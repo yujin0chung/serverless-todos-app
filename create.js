447ecf2cea60b4fc1f14833f46a27df3d2c58245
yujin <yujin@yujins-MacBook-Air.local>
2020-10-26 15:01:41 -0700

    configure aws lambda and add post route

---
+++ create.js
@@ -0,0 +1,26 @@
+const AWS = require("aws-sdk");
+const client = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require("uuid");
+
+module.exports.run = async (event) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "todos",
+    Item: {
+      id: uuidv4(),
+      text: data.text,
+      checked: false,
+    },
+  };
+  try {
+    await client.put(params).promise();
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/plain" },
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    console.log("Error occurred creating todo: ", err);
+    throw err;
+  }
+};
